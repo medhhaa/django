f6bf7c0ca41cb2e19e013fbe61f74f3c8baced9a
Medha Khatri <81421540+medhhaa@users.noreply.github.com>
2025-03-26 12:40:30 -0400

    polls app

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,5 +1,45 @@
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     # the context is a dictionary mapping template variable names to Python objects.
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+
+# use helper function shortcut to maintain the MVC - Model View Controller and keep it loosely coupled
+# Models: deals with databases
+# Views: deals with HTTP requests
+# if you raise manual exceptions, you are adding dependency in the model layer to deal with views!
+# use shortcut
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
